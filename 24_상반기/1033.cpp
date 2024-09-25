e0baa1a38ccc0522a5d21080904d542db0d72e0b
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-09-25 20:08:31 +0900

    fixed

--- 24_상반기/1033.cpp
+++ 24_상반기/1033.cpp
@@ -1,17 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0)
 long long A = 1;
-long long gcd(long long a, long long b){
-    if(b==0) return a;
-    return  gcd(b, a % b);
+long long gcd(long long a, long long b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
 }
 long long arr[10];
-long long lcm(long long a, long long b){
+long long lcm(long long a, long long b)
+{
     return a * b / gcd(a, b);
 }
 
-struct node{
+struct node
+{
     int to;
     long long m, d;
 };
@@ -19,16 +26,18 @@ struct node{
 vector<node> adj[10];
 int N;
 
-int main(){
+int main()
+{
     fastio;
     cin >> N;
-    for (int i = 0; i < N-1; i++)
+    for (int i = 0; i < N - 1; i++)
     {
         int f, t, m, d;
         cin >> f >> t >> m >> d;
         long long mod = gcd(m, d);
-        m /= mod; d /= mod;
-        A *= lcm(m ,d);
+        m /= mod;
+        d /= mod;
+        A *= lcm(m, d);
         adj[f].push_back({t, d, m});
         adj[t].push_back({f, m, d});
     }
@@ -37,13 +46,15 @@ int main(){
     arr[0] = A;
     while (!q.empty())
     {
-        auto cur = q.front(); q.pop();
+        auto cur = q.front();
+        q.pop();
         for (auto nxt : adj[cur])
         {
-            if(arr[nxt.to]) continue;
+            if (arr[nxt.to])
+                continue;
             arr[nxt.to] = (arr[cur] / nxt.d) * nxt.m;
-            q.push({nxt.to});            
-        }        
+            q.push({nxt.to});
+        }
     }
     for (int i = 0; i < N; i++)
     {
