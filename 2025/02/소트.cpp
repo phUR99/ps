b13746497a1b3cccf2bac1294f1166011882a0b9
phUR99 <122969226+phUR99@users.noreply.github.com>
2025-02-13 23:37:04 +0900

    250213

---
+++ 2025/02/소트.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+#define ll long long
+int cnt[1005];
+int n;
+
+int main()
+{
+    fastio;
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int idx;
+        cin >> idx;
+        cnt[idx]++;
+    }
+    while (n--)
+    {
+        for (int i = 0; i <= 1000; i++)
+        {
+            if (!cnt[i])
+                continue;
+            if (cnt[i + 1])
+            {
+                int M = 0;
+                // 실수 2
+                for (int j = i + 2; j <= 1000; j++)
+                {
+                    if (cnt[j])
+                    {
+                        M = j;
+                        break;
+                    }
+                }
+
+                if (M == 0)
+                    continue;
+                while (cnt[i] > 0)
+                {
+                    cout << i << ' ';
+                    cnt[i]--;
+                }
+                cout << M << ' ';
+                cnt[M]--;
+            }
+            else
+            {
+                cout << i << ' ';
+                cnt[i]--;
+                // 실수 1
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
