1fe4454328b317274b57f9d3fd8dcd27f5b9304c
phUR99 <skerrigen12@gmail.com>
2025-04-10 16:18:56 +0900

    250410

---
+++ 2025/04/현욱은_괄호왕이야.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+#define ll long long
+int n;
+string str;
+stack<int> s;
+
+
+int main()
+{
+    fastio;
+
+    
+
+    return 0;
+}
